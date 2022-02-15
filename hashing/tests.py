db9ab7827ecce727c926b18fd9984583ea097e69
mslmyrtd <mslmyrtd@gmail.com>
2022-02-15 17:34:47 +0300

    model test edildi

--- hashing/tests.py
+++ hashing/tests.py
@@ -1,11 +1,32 @@
 
+import hashlib
 from django.test import TestCase
 from selenium import webdriver
 import time
+from hashing.forms import HashForm
+from .models import Hash
 class UnitTestCase(TestCase):
     def test_home_homepage_template(self):
         response=self.client.get("/")
         self.assertTemplateUsed(response,"hashing/home.html")
+    def test_hash_form(self):
+        form = HashForm(data={"text":"hello"})
+        self.assertTrue(form.is_valid())
+        
+    def test_hash_func_works(self):
+        hash=hashlib.sha256("hello" .encode("utf-8")).hexdigest()
+        self.assertEqual('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824',hash)
+        
+    def test_model_hash(self):
+        hash=Hash()
+        hash.text="hello"
+        hash.hash = '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824'
+        hash.save()
+        pulled_hash = Hash.objects.get(hash='2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824')
+        self.assertEqual(hash.text,pulled_hash.text)
+        
+        
+        
 
 # class functionalTestCase(TestCase):
 #     def setUp(self):
