115928e7955a3e150a134b074f9579894c39fab6
mslmyrtd <mslmyrtd@gmail.com>
2022-02-15 17:14:24 +0300

    hash generator control edildi

--- hashing/tests.py
+++ hashing/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from selenium import webdriver
-
+import time
 class UnitTestCase(TestCase):
     pass
 
@@ -14,4 +14,12 @@ class functionalTestCase(TestCase):
         self.assertIn("Enter hash here",self.browser.page_source)
 
 
+def test_hash_of_hello(self):
+    self.browser.get('http://localhost:8000')
+    text = self.browser.find_element_by_id("id_text")
+    text.send_keys("hello")
+    time.sleep(1)
+    self.browser.find_element_by_name("submit").click()
+    self.assertInHTML('f24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', self.browser.page_source)
+    time.sleep(2)
 
