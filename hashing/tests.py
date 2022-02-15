26c956941eaa6e2b6ddab0ea32c5b9184998c69b
mslmyrtd <mslmyrtd@gmail.com>
2022-02-15 16:38:15 +0300

    test.py de functional testcase create edildi run

--- hashing/tests.py
+++ hashing/tests.py
@@ -1,1 +1,17 @@
+from django.test import TestCase
+from selenium import webdriver
+
+class UnitTestCase(TestCase):
+    pass
+
+class functionalTestCase(TestCase):
+    def setUp(self):
+        self.browser=webdriver.Firefox()
+    def tearDown(self):
+        self.browser.quit()
+    def test_ther_is_homepage(self):
+        self.browser.get("http://localhost:8000")
+        self.assertIn("Enter hash here",self.browser.page_source)
+
+
 
